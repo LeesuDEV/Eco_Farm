61c0342455d426e28ade048abf0a45f41844050f
LeesuDEV <tngus9753141@gmail.com>
2024-07-06 20:32:51 +0900

    프리셋허브 : 글 조회후 해당글 삭제시 Listener에서 NullPointerException 뜨던오류 수정

--- app/src/main/java/com/example/proto/PresetHubDetailDialog.java
+++ app/src/main/java/com/example/proto/PresetHubDetailDialog.java
@@ -2,6 +2,7 @@ package com.example.proto;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -20,6 +21,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -28,15 +30,17 @@ import java.util.Map;
 
 public class PresetHubDetailDialog extends Dialog {
     Context context;
-    TextView PresetNameTV,PresetCommentTV;
-    TextView Preset1WeekTempTV,Preset1WeekHumTV,Preset1WeekBrightnessTV;
-    TextView Preset2WeekTempTV,Preset2WeekHumTV,Preset2WeekBrightnessTV;
-    TextView Preset3WeekTempTV,Preset3WeekHumTV,Preset3WeekBrightnessTV;
-    TextView Preset4WeekTempTV,Preset4WeekHumTV,Preset4WeekBrightnessTV;
+    TextView PresetNameTV, PresetCommentTV;
+    TextView Preset1WeekTempTV, Preset1WeekHumTV, Preset1WeekBrightnessTV;
+    TextView Preset2WeekTempTV, Preset2WeekHumTV, Preset2WeekBrightnessTV;
+    TextView Preset3WeekTempTV, Preset3WeekHumTV, Preset3WeekBrightnessTV;
+    TextView Preset4WeekTempTV, Preset4WeekHumTV, Preset4WeekBrightnessTV;
     TextView PresetHitTV;
     TextView PresetSubmitBtn;
     TextView hitTV;
     int selectNum;
+
+    ListenerRegistration registration;
 
     public PresetHubDetailDialog(Context context) {
         super(context);
@@ -75,38 +79,38 @@ public class PresetHubDetailDialog extends Dialog {
                 String name = PresetNameTV.getText().toString();
                 int value = Integer.parseInt(PresetHitTV.getText().toString());
 
-                if(hitTV.getText().toString().equals("♡")){
-                    value ++;
+                if (hitTV.getText().toString().equals("♡")) {
+                    value++;
 
-                    Map<String,Object>obj = new HashMap<>();
-                    obj.put("hit",value);
+                    Map<String, Object> obj = new HashMap<>();
+                    obj.put("hit", value);
 
                     MainFragment.cropsHub_DB.document(name).update(obj).addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
                         public void onSuccess(Void unused) {
-                            Toast.makeText(context,"이 글을 좋아합니다",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(context, "이 글을 좋아합니다", Toast.LENGTH_SHORT).show();
                         }
                     });
 
-                    Map<String,Object>hitObj = new HashMap<>();
-                    hitObj.put(String.valueOf(selectNum),true);
+                    Map<String, Object> hitObj = new HashMap<>();
+                    hitObj.put(String.valueOf(selectNum), true);
 
                     MainFragment.fireStore_MyDB.collection("preset").document("hitPreset").update(hitObj);
                 } else {
-                    value --;
+                    value--;
 
-                    Map<String,Object>obj = new HashMap<>();
-                    obj.put("hit",value);
+                    Map<String, Object> obj = new HashMap<>();
+                    obj.put("hit", value);
 
                     MainFragment.cropsHub_DB.document(name).update(obj).addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
                         public void onSuccess(Void unused) {
-                            Toast.makeText(context,"좋아요를 취소합니다",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(context, "좋아요를 취소합니다", Toast.LENGTH_SHORT).show();
                         }
                     });
 
-                    Map<String,Object>hitObj = new HashMap<>();
-                    hitObj.put(String.valueOf(selectNum),false);
+                    Map<String, Object> hitObj = new HashMap<>();
+                    hitObj.put(String.valueOf(selectNum), false);
 
                     MainFragment.fireStore_MyDB.collection("preset").document("hitPreset").update(hitObj);
                 }
@@ -121,26 +125,33 @@ public class PresetHubDetailDialog extends Dialog {
             }
         });
 
-         loadPresetDetail(); //세부정보 가져오기
+        loadPresetDetail(); //세부정보 가져오기
     }
 
-    private void loadPresetDetail(){
-        if (PresetHubDialog.selectedItem == null){
-            Toast.makeText(context,"No selected Item",Toast.LENGTH_SHORT).show();
+    @Override
+    public void dismiss() {
+        super.dismiss();
+        if (registration != null){
+            registration.remove();
+        }
+    }
+
+    private void loadPresetDetail() {
+        if (PresetHubDialog.selectedItem == null) {
+            Toast.makeText(context, "No selected Item", Toast.LENGTH_SHORT).show();
             return;
         }
 
 
-
-        MainFragment.cropsHub_DB.document(PresetHubDialog.selectedItem).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+        registration = MainFragment.cropsHub_DB.document(PresetHubDialog.selectedItem).addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
                 if (error != null) {
-                    Log.d("error","failed");
+                    Log.d("error", "failed");
                     return;
                 }
                 if (value == null) {
-                    Log.d("empty","empty");
+                    Log.d("empty", "empty");
                 }
                 PresetNameTV.setText(value.getId());
                 PresetCommentTV.setText(value.get("comment").toString());
@@ -150,23 +161,23 @@ public class PresetHubDetailDialog extends Dialog {
                 String[] week3 = value.get("3week").toString().split(";");
                 String[] week4 = value.get("4week").toString().split(";");
 
-                Preset1WeekTempTV.setText(week1[0]+"˚C");
-                Preset1WeekHumTV.setText(week1[1]+"%");
-                Preset1WeekBrightnessTV.setText(week1[2]+"%");
+                Preset1WeekTempTV.setText(week1[0] + "˚C");
+                Preset1WeekHumTV.setText(week1[1] + "%");
+                Preset1WeekBrightnessTV.setText(week1[2] + "%");
 
-                Preset2WeekTempTV.setText(week2[0]+"˚C");
-                Preset2WeekHumTV.setText(week2[1]+"%");
-                Preset2WeekBrightnessTV.setText(week2[2]+"%");
+                Preset2WeekTempTV.setText(week2[0] + "˚C");
+                Preset2WeekHumTV.setText(week2[1] + "%");
+                Preset2WeekBrightnessTV.setText(week2[2] + "%");
 
-                Preset3WeekTempTV.setText(week3[0]+"˚C");
-                Preset3WeekHumTV.setText(week3[1]+"%");
-                Preset3WeekBrightnessTV.setText(week3[2]+"%");
+                Preset3WeekTempTV.setText(week3[0] + "˚C");
+                Preset3WeekHumTV.setText(week3[1] + "%");
+                Preset3WeekBrightnessTV.setText(week3[2] + "%");
 
-                Preset4WeekTempTV.setText(week4[0]+"˚C");
-                Preset4WeekHumTV.setText(week4[1]+"%");
-                Preset4WeekBrightnessTV.setText(week4[2]+"%");
+                Preset4WeekTempTV.setText(week4[0] + "˚C");
+                Preset4WeekHumTV.setText(week4[1] + "%");
+                Preset4WeekBrightnessTV.setText(week4[2] + "%");
 
-                PresetHitTV.setText(""+value.get("hit").toString());
+                PresetHitTV.setText("" + value.get("hit").toString());
 
                 //글번호로 좋아요를 판별하기 위한 변수
                 selectNum = (int) value.getLong("number").longValue();
@@ -177,9 +188,9 @@ public class PresetHubDetailDialog extends Dialog {
                         String num = String.valueOf(selectNum);
 
                         // 글을 본적이 있나요?
-                        if (mValue.contains(num)){
+                        if (mValue.contains(num)) {
                             // 이미 내가 좋아요를 누른 글이라면? (true)
-                            if (mValue.getBoolean(num)){
+                            if (mValue.getBoolean(num)) {
                                 hitTV.setText("♥");
                             } else {
                                 hitTV.setText("♡");
