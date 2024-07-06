1707946804f00a915b3f41afc1e0d975526b907d
LeesuDEV <tngus9753141@gmail.com>
2024-07-06 16:53:06 +0900

    프리셋 : 허브에 수정기능 추가완료

--- app/src/main/java/com/example/proto/PresetFixDialog.java
+++ app/src/main/java/com/example/proto/PresetFixDialog.java
@@ -5,14 +5,21 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class PresetFixDialog extends Dialog {
 
@@ -24,6 +31,7 @@ public class PresetFixDialog extends Dialog {
     EditText Preset3WeekTempTV, Preset3WeekHumTV, Preset3WeekBrightnessTV;
     EditText Preset4WeekTempTV, Preset4WeekHumTV, Preset4WeekBrightnessTV;
     TextView week1TV, week2TV, week3TV, week4TV;
+    TextView PresetFixSubmitBtn,PresetHubRegisterBtn;
 
 
     public PresetFixDialog(Context context) {
@@ -56,6 +64,17 @@ public class PresetFixDialog extends Dialog {
         week2TV = findViewById(R.id.week2TV);
         week3TV = findViewById(R.id.week3TV);
         week4TV = findViewById(R.id.week4TV);
+
+        PresetFixSubmitBtn = findViewById(R.id.PresetFixSubmitBtn);
+        PresetHubRegisterBtn = findViewById(R.id.PresetHubRegisterBtn);
+
+        PresetFixSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                applyEditPreset(); //프리셋 업데이트
+            }
+        });
+
 
         loadMyPreset();
     }
@@ -124,6 +143,51 @@ public class PresetFixDialog extends Dialog {
                         Preset1WeekBrightnessTV.setTextColor(Color.parseColor("#78FF6E"));
                         break;
                 }
+            }
+        });
+    }
+
+    public void applyEditPreset() {
+        String name = PresetNameTV.getText().toString();
+        String comment = PresetCommentTV.getText().toString();
+
+        String week1_temp = Preset1WeekTempTV.getText().toString();
+        String week1_hum = Preset1WeekHumTV.getText().toString();
+        String week1_brightness = Preset1WeekBrightnessTV.getText().toString();
+        String week2_temp = Preset2WeekTempTV.getText().toString();
+        String week2_hum = Preset2WeekHumTV.getText().toString();
+        String week2_brightness = Preset2WeekBrightnessTV.getText().toString();
+        String week3_temp = Preset3WeekTempTV.getText().toString();
+        String week3_hum = Preset3WeekHumTV.getText().toString();
+        String week3_brightness = Preset3WeekBrightnessTV.getText().toString();
+        String week4_temp = Preset4WeekTempTV.getText().toString();
+        String week4_hum = Preset4WeekHumTV.getText().toString();
+        String week4_brightness = Preset4WeekBrightnessTV.getText().toString();
+
+        String week1 = week1_temp+";"+week1_hum+";"+week1_brightness;
+        String week2 = week2_temp+";"+week2_hum+";"+week2_brightness;
+        String week3 = week3_temp+";"+week3_hum+";"+week3_brightness;
+        String week4 = week4_temp+";"+week4_hum+";"+week4_brightness;
+
+        //텍스트 중 하나라도 비어있다면.
+        if (name.isEmpty() || comment.isEmpty() || week1_hum.isEmpty() || week1_brightness.isEmpty() || week1_temp.isEmpty() || week2_brightness.isEmpty() || week2_hum.isEmpty() || week2_temp.isEmpty() || week3_temp.isEmpty() || week3_hum.isEmpty() || week3_brightness.isEmpty() || week4_brightness.isEmpty() || week4_hum.isEmpty() || week4_temp.isEmpty()){
+            Toast.makeText(context,"빈칸이 존재해요!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Map에 데이터 준비
+        Map<String, Object>obj = new HashMap<>();
+        obj.put("name",name);
+        obj.put("comment",comment);
+        obj.put("1week",week1);
+        obj.put("2week",week2);
+        obj.put("3week",week3);
+        obj.put("4week",week4);
+
+        MainFragment.fireStore_MyDB.collection("preset").document("preset").update(obj).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(context,"프리셋 저장에 성공했어요!",Toast.LENGTH_SHORT).show();
             }
         });
     }
